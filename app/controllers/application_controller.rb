4c111fed7d1f88fd1733eab0debe8f5bd1998602
Doug Whetter <doug.whetter@appfolio.com>
2019-12-10 06:50:01 -0800

    Updated application controller and tests to support image list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def home
-    redirect_to new_image_path
+  def home;
+    @images = Image.all
   end
 end
