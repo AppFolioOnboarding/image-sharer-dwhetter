9ba3098de5be0d35083c6197e476806540921604
Doug Whetter <doug.whetter@appfolio.com>
2019-12-10 06:50:01 -0800

    Updated application controller and tests to support image list

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,7 +3,14 @@ require 'test_helper'
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test 'test application controller' do
     get '/'
-    assert_response 302
-    assert_select 'body', 'You are being redirected.'
+    assert_response 200
+    assert_select 'h1', 'Image List'
+  end
+
+  test 'test image count' do
+    get '/'
+    assert_select 'img' do |image|
+      assert_select image, 'img', 2
+    end
   end
 end
---
+++ test/system/applications_test.rb
@@ -0,0 +1,26 @@
+require 'application_system_test_case'
+
+class ApplicationTest < ApplicationSystemTestCase
+  setup do
+    @image = images(:one)
+  end
+
+  test 'storing an Image URL' do
+    visit new_image_path
+    @image.url = 'https://test'
+
+    fill_in 'image[url]', with: @image.url
+    click_on 'Store URL'
+    assert_selector 'p', text: 'Image was successfully created'
+  end
+
+  test 'storing an invalid Image URL' do
+    visit new_image_path
+    @image.url = 'http//test'
+
+    fill_in 'image[url]', with: @image.url
+    click_on 'Store URL'
+    assert_selector 'h2', text: '1 error prohibited this image from being saved:'
+    assert_selector 'li', text: 'Url is invalid'
+  end
+end
