742848e7ee4b135838a5a5c97bf49879332259cb
doug.whetter <doug.whetter@appfolio.com>
2019-12-11 06:02:52 -0800

    dw - added 20 default urls to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Image.create([
+  { url: 'https://media.mnn.com/assets/images/2019/04/black-hole-photo-zoom.jpg.838x0_q80.jpg' },
+  { url: 'http://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg' },
+  { url: 'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg' },
+  { url: 'https://www.w3schools.com/w3css/img_lights.jpg' },
+  { url: 'https://www.w3schools.com/howto/img_snow.jpg' },
+  { url: 'https://cdn.cnn.com/cnnnext/dam/assets/181215042152-nasa-juno-01-large-169.jpg' },
+  { url: 'https://i.dailymail.co.uk/i/pix/2016/11/02/15/39FD647300000578-0-image-a-15_1478101071773.jpg' },
+  { url: 'https://www.w3schools.com/howto/img_snow.jpg' },
+  { url: 'https://media.gettyimages.com/photos/caribbean-paradise-picture-id506190774?s=612x612' },
+  { url: 'https://www.w3schools.com/w3css/img_lights.jpg' },
+  { url: 'https://www.tonyseel.com/wp-content/uploads/2019/04/water-image.jpg' },
+  { url: 'https://www.istockphoto.com/resources/images/PhotoFTLP/NatureLandscapes-519760984.jpg' },
+  { url: 'http://sostav.ua/app/public/images/news/2019/02/19/5JuZMmTY_l.jpg' },
+  { url: 'https://www.w3schools.com/w3css/img_lights.jpg' },
+  { url: 'http://www.baltana.com/files/wallpapers-1/Island-HD-Wallpapers-03420.jpg' },
+  { url: 'https://www.w3schools.com/howto/img_snow.jpg' },
+  { url: 'https://www.tonyseel.com/wp-content/uploads/2019/04/water-image.jpg' },
+  { url: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/10D88/production/_103600096_mediaitem103600095.jpg' },
+  { url: 'https://cdn.cnn.com/cnnnext/dam/assets/181215042152-nasa-juno-01-large-169.jpg' },
+  { url: 'https://cdn.pixabay.com/photo/2015/12/01/20/28/fall-1072821__340.jpg' }
+])
