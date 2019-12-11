81059c5b59b8c825cfc7392bf1be9bd1cc03b482
doug.whetter <doug.whetter@appfolio.com>
2019-12-11 11:33:42 -0800

    updated per rubocop suggestion of a guard clause

--- test/system/images_test.rb
+++ test/system/images_test.rb
@@ -14,6 +14,17 @@ class ImagesTest < ApplicationSystemTestCase
     assert_selector 'p', text: 'Image was successfully created'
   end
 
+  test 'storing an Image URL with a tag' do
+    visit new_image_path
+    @image.url = 'https://test'
+
+    fill_in 'image[url]', with: @image.url
+    fill_in 'image[tag_list]', with: 'imageTag'
+    click_on 'Store URL'
+    assert_selector 'p', text: 'Image was successfully created'
+    assert_selector 'body', text: 'imageTag'
+  end
+
   test 'storing an invalid Image URL' do
     visit new_image_path
     @image.url = 'http//test'
