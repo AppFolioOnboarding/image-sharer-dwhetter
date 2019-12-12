d9074fcad832c6b01c1485d4086d8b525287b410
doug.whetter <doug.whetter@appfolio.com>
2019-12-11 16:29:40 -0800

    dw - added ability to filter by tag and display tagged images

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -9,8 +9,20 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
 
   test 'test image count' do
     get '/'
-    assert_select 'img' do |image|
-      assert_select image, 'img', 2
-    end
+    assert_select 'img', count: 2
+  end
+
+  test 'test image with no valid tag' do
+    get '/?tag_list=no_tag'
+    assert_select 'img', count: 0
+    assert_select 'h2', text: 'No images found.'
+  end
+
+  test 'test image with valid tag' do
+    Image.create!(url: 'https://my_image_url1', tag_list: 'test')
+    Image.create!(url: 'https://my_image_url2', tag_list: 'test')
+    Image.create!(url: 'https://my_image_url3', tag_list: 'test')
+    get '/?tag_list=test'
+    assert_select 'img', count: 3
   end
 end
